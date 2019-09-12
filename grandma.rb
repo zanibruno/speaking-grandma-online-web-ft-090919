488050fdf47cfa474b4edfb1feedefda989ff5a9
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-12 08:41:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,14 +8,4 @@ def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
 end 
 end 
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# Whatever you say to grandma, she shoul
